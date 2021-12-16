cc73eebf7a8e5f0ec8bd65a8666f19f0e01f82b8
bellabula <zz225864@gmail.com>
2021-12-16 16:53:45 +0800

    list comprehension

--- read.py
+++ read.py
@@ -11,8 +11,8 @@ print('檔案中總共有', len(data), '筆資料')
 
 # 留言平均長度
 sum_len = 0
-for c in data:
-    sum_len += len(c)
+for d in data:
+    sum_len += len(d)
 avg_len = sum_len/len(data)
 print('所有留言的平均長度為', avg_len)
 
@@ -22,3 +22,18 @@ for d in data:
     if len(d) < 100:
         new.append(d)
 print('總共有',len(new), '筆留言長度小於100')
+
+
+# 含有good的留言
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(d)
+print('總共有',len(good), '筆留言提到"good"')
+# list comprehension
+good_c = [d for d in data if 'good' in d]
+print(len(good_c))
+
+# 留言中是否含有'bad', True Fales
+bad = ['bad' in d for d in data]
+print('總共有',sum(bad), '筆留言中包含"bad"')
