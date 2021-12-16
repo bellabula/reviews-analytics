fc4edff04023e6cfa0bb47882e5ab0ea7c591ca5
bellabula <zz225864@gmail.com>
2021-12-16 16:20:30 +0800

    add filter fun

--- read.py
+++ read.py
@@ -1,3 +1,4 @@
+# iput 留言資料
 data = []
 count = 0
 with open('reviews.txt', 'r') as f:
@@ -6,11 +7,18 @@ with open('reviews.txt', 'r') as f:
         count += 1
         if count % 10000 == 0:
             print(len(data))
-
 print('檔案中總共有', len(data), '筆資料')
 
+# 留言平均長度
 sum_len = 0
 for c in data:
     sum_len += len(c)
+avg_len = sum_len/len(data)
+print('所有留言的平均長度為', avg_len)
 
-print('所有留言的平均長度為', sum_len/len(data))
+# 留言長度 < 100
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('總共有',len(new), '筆留言長度小於100')
